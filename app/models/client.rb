0746e8e5280f6b3e4d73b13d8cb3a9c0043a9f71
hkjx <humanresourcett@gmail.com>
2013-07-19 13:40:25 +0300

    added convertion options for clients images

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,7 @@
 class Client < ActiveRecord::Base
   attr_accessible :title, :image
-  has_attached_file :image, :styles => { :medium => "186x80!", :thumb => "100x100>" }
+  convert_options_prefix = "-background transparent -compose Copy -gravity center -extent"
+  has_attached_file :image, :styles => { :medium => "186x80>", :thumb => "100x100>" },
+  :convert_options => { :medium => "#{convert_options_prefix} 186x80", :thumb => "#{convert_options_prefix} 100x100" }
 
 end
